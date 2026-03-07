92106be87bd424e44f3b19523e346523d3ce46ef
Felix <191755642+felexcoding@users.noreply.github.com>
2026-03-08 01:33:11 +0300

    Update device.mk

--- device.mk
+++ device.mk
@@ -3,7 +3,7 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-LOCAL_PATH := device/generic/boot
+LOCAL_PATH := device/vivo/PD2353
 
 # For building with minimal manifest
 ALLOW_MISSING_DEPENDENCIES := true
