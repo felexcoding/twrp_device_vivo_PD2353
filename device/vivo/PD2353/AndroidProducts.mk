dcf15b39eed6bfcc25a20bc6562ac1da3e25a137
qcopy-alt <191755642+felexcoding@users.noreply.github.com>
2026-03-09 18:32:08 +0300

    ehm

--- device/vivo/PD2353/AndroidProducts.mk
+++ device/vivo/PD2353/AndroidProducts.mk
@@ -7,7 +7,7 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/omni_boot.mk
 
 COMMON_LUNCH_CHOICES := \
-    omni_boot-user \
-    omni_boot-userdebug \
-    omni_boot-eng
+    omni_PD2353-user \
+    omni_PD2353-userdebug \
+    omni_PD2353-eng
 
--- device/vivo/PD2353/omni_boot.mk
+++ device/vivo/PD2353/omni_boot.mk
@@ -7,14 +7,13 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product-if-exists, vendor/twrp/config/common.mk)
+$(call inherit-product-if-exists, vendor/omni/config/common.mk)
 
-# Inherit from device config
 $(call inherit-product, device/vivo/PD2353/device.mk)
 
 PRODUCT_DEVICE := PD2353
-PRODUCT_NAME := omni_boot
+PRODUCT_NAME := omni_PD2353
 PRODUCT_BRAND := vivo
 PRODUCT_MODEL := PD2353
 PRODUCT_MANUFACTURER := vivo
