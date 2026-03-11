b80ccabebacfb1e49d1f46b7937462b4deb1eaf7
qcopy-alt <191755642+felexcoding@users.noreply.github.com>
2026-03-11 13:17:26 +0300

    test

--- device/vivo/PD2353/device.mk
+++ device/vivo/PD2353/device.mk
@@ -3,3 +3,21 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
+# A/B support
+AB_OTA_PARTITIONS += \
+    boot \
+    system \
+    vendor_boot
+
+# Prebuilt kernel/dtb
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/prebuilt/kernel:kernel \
+    $(DEVICE_PATH)/prebuilt/dtb.img:dtb.img
+
+# TWRP
+PRODUCT_PACKAGES += \
+    recovery
+
+# Screen density
+PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREF_CONFIG := xxhdpi
--- device/vivo/PD2353/omni_PD2353.mk
+++ device/vivo/PD2353/omni_PD2353.mk
@@ -4,8 +4,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+$(call inherit-product, device/vivo/PD2353/device.mk)
+$(call inherit-product, vendor/omni/config/common.mk)
 
 PRODUCT_DEVICE := PD2353
 PRODUCT_NAME := omni_PD2353
