b18525024bd845485ad1e78a39594ff2a3667c05
qcopy-alt <191755642+felexcoding@users.noreply.github.com>
2026-03-11 14:58:43 +0300

    c

--- device/vivo/PD2353/Android.mk
+++ device/vivo/PD2353/Android.mk
@@ -7,5 +7,10 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),PD2353)
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
+
+$(PRODUCT_OUT)/dtb.img: $(LOCAL_PATH)/prebuilt/dtb.img
+	@echo "Copying dtb.img to $(PRODUCT_OUT)"
+	$(hide) cp $< $@
+
 endif
 
